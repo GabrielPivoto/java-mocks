7cef4480d7d6b8361350c20ebd943649c79a62dd
Gabriel Pivoto Ambrósio <gabriel-pivoto@outlook.com>
2022-09-23 10:42:23 -0300

    Adicionando a anotação Captor e criando mais um teste do Gerador de Pagamentos

--- src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
+++ src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
@@ -1,7 +1,54 @@
 package br.com.alura.leilao.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import br.com.alura.leilao.dao.LeilaoDao;
+import br.com.alura.leilao.dao.PagamentoDao;
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Pagamento;
+import br.com.alura.leilao.model.Usuario;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 class GeradorDePagamentoTest {
+
+    private GeradorDePagamento gerador;
+    @Mock
+    private PagamentoDao dao;
+
+    @Captor
+    private ArgumentCaptor<Pagamento> captor;
+
+    @BeforeEach
+    public void beforeEach(){
+        MockitoAnnotations.initMocks(this);
+        this.gerador = new GeradorDePagamento(dao);
+    }
+
+    @Test
+    void deveriaCriarPagamentoParaVencedorDoLeilao(){
+        Leilao leilao = leilao();
+        Lance lanceVencedor = leilao.getLanceVencedor();
+        gerador.gerarPagamento(lanceVencedor);
+
+        Mockito.verify(dao).salvar(captor.capture()); // captor.capture() captura o objeto passado como parametro
+
+        Pagamento pagamento = captor.getValue(); // captor.getValue() devolve o objeto capturado
+    }
+
+    private Leilao leilao() {
+        Leilao leilao = new Leilao("Celular",new BigDecimal("500"), new Usuario("Fulano"));
+
+        Lance lance = new Lance(new Usuario("Ciclano"), new BigDecimal("900"));
+
+        leilao.propoe(lance);
+        leilao.setLanceVencedor(lance);
+
+        return leilao;
+    }
 
 }
